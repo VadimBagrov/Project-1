c7b5b5a2053739bc7a0e1874d5ee4278e1cb8151
Night Developer <110101692+VadimBagrov@users.noreply.github.com>
2024-04-29 22:20:39 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,49 @@
+import Swiper from 'swiper'
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
+import 'swiper/css'
+
+const swiper = new Swiper('.swiper', {
+  direction: 'vertical',
+  loop: true,
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
+const button = document.querySelector('.catalog__more'); //ищем кнопку
+const menu = document.querySelector('.catalog__bottom-bl'); //ищем в DOM элемент с классом навигации, которая скрыта за бургером
+const cat = document.querySelector('.catalog');
+// создаем функцию-хэндлер для смены класса
+
+const onToggleMenuHandler = () => menu.classList.toggle('catalog__bottom-bl--active'); // тут добавляешь класс к меню, который будет открывать это меню
+
+const height = () => cat.classList.toggle('catalog--active');
+
+button.addEventListener('click', onToggleMenuHandler); //накидываем на кнопку обработчик событий по клику и передаем вторым аргументом нашу функцию.
+
+button.addEventListener('click', height);
+
+document.querySelector('.service__dvs').onclick = function () {
+  document.querySelector('.service__general').classList.add('service__general--none');
+  document.querySelector('.service__transmission').classList.add('service__transmission--none');
+  document.querySelector('.service__obslug').classList.add('service__obslug--none');
+  document.querySelector('.service__dvigatel').classList.remove('service__dvigatel--none');
+}
+
+document.querySelector('.service__tran').onclick = function () {
+  document.querySelector('.service__general').classList.add('service__general--none');
+  document.querySelector('.service__dvigatel').classList.add('service__dvigatel--none');
+  document.querySelector('.service__obslug').classList.add('service__obslug--none');
+  document.querySelector('.service__transmission').classList.remove('service__transmission--none');
+}
+
+document.querySelector('.service__obsl').onclick = function () {
+  document.querySelector('.service__general').classList.add('service__general--none');
+  document.querySelector('.service__dvigatel').classList.add('service__dvigatel--none');
+  document.querySelector('.service__transmission').classList.add('service__transmission--none');
+  document.querySelector('.service__obslug').classList.remove('service__obslug--none');
+}
