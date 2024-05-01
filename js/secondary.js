698822217e3658410f89a723ebffbcd581e635cf
Night Developer <110101692+VadimBagrov@users.noreply.github.com>
2024-05-02 01:05:22 +0300

    Add files via upload

--- js/secondary.js
+++ js/secondary.js
@@ -41,6 +41,40 @@ $(".multiple-items").slick({
   slidesToShow: 5,
   slidesToScroll: 1,
   variableWidth: true,
+  prevArrow: $('.mit__prev'),
+  nextArrow: $('.mit__next'),
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 654,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
+$(".multiple-items2").slick({
+  speed: 1000,
+  autoplay: true,
+  autoplaySpeed: 1500,
+  slidesToShow: 5,
+  slidesToScroll: 1,
   prevArrow: $('.prev'),
   nextArrow: $('.next'),
   responsive: [
@@ -69,6 +103,10 @@ $(".multiple-items").slick({
   ]
 });
 
+document.querySelector('.seo__bolee').addEventListener('click', () => {
+  document.querySelector('.seo__text').classList.add('seo__text--active');
+  document.querySelector('.seo__bolee').classList.add('seo__bolee--none');
+})
 const button = document.querySelector('.catalog__more'); //ищем кнопку
 const menu = document.querySelector('.catalog__bottom-bl'); //ищем в DOM элемент с классом навигации, которая скрыта за бургером
 const cat = document.querySelector('.catalog');
@@ -76,24 +114,13 @@ const cat = document.querySelector('.catalog');
 
 const height = () => cat.classList.toggle('catalog--active');
 
-document.querySelector('.catalog__more').onclick = function () {
-  document.querySelector('.catalog__bottom-bl').classList.toggle('catalog__bottom-bl--active');
-  document.querySelector('.catalog__more').classList.add('catalog__more--none');
-  document.querySelector('.catalog__mob').classList.add('catalog__bottom-bl--active');
-}
 
-document.querySelector('.seo__bolee').onclick = function () {
-  document.querySelector('.seo__text ').classList.add('seo__text--active');
-  document.querySelector('.seo__bolee').classList.add('seo__bolee--none');
-}
-
-button.addEventListener('click', height);
-
-
-document.querySelector('.header-mb__menu-btn').onclick = function () {
-  document.querySelector('.header__pop').classList.toggle('header__pop--none');
-}
-
+document.querySelector('.service__dvs').addEventListener('click', () => {
+  document.querySelector('.service__general').classList.add('service__general--none');
+  document.querySelector('.service__transmission').classList.add('service__transmission--none');
+  document.querySelector('.service__obslug').classList.add('service__obslug--none');
+  document.querySelector('.service__dvigatel').classList.remove('service__dvigatel--none');
+})
 
 document.querySelector('.header-mb__menu-btn').addEventListener('click', () => {
   document.querySelector('.header__pop').classList.toggle('header__pop--none');
