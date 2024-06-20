fd047191748953ff1c08693b5bf6067ab5021534
Night Developer <110101692+VadimBagrov@users.noreply.github.com>
2024-06-20 18:45:00 +0300

    Add files via upload

---
+++ voltgreen calculate/main.js
@@ -0,0 +1,13 @@
+$('#dark').hide();
+$('.add').hide();
+
+$('#right-add').click(function () {
+  $('#dark').show();
+  $('.add').show();
+});
+
+
+$('#add-btn').click(function () {
+  $('#dark').hide();
+  $('.add').hide();
+});
