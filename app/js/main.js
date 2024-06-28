6aedb251db4a38b8a9d75c52e602cd82bd3f5a4a
Night Developer <110101692+VadimBagrov@users.noreply.github.com>
2024-06-28 23:30:18 +0300

    Add files via upload

--- app/js/main.js
+++ app/js/main.js
@@ -6,8 +6,8 @@ $('.usl-slider').slick({
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 3000,
-  prevArrow: $('.prev'),
-  nextArrow: $('.next'),
+  prevArrow: $('.prev10'),
+  nextArrow: $('.next10'),
   responsive: [
     {
       breakpoint: 1024,
@@ -193,14 +193,174 @@ $('.variable-width').slick({
   autoplaySpeed: 1500
 });
 
-$('house__btn--active').slick({
-
+$('.otzyv-s').slick({
+  dots: true,
+  speed: 1500,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
 });
 
 $('.house__close--active').click(function () {
-  $('#house__active').hide();
+  $('.house__slide1--active').hide();
 });
 
 $('.house__btn').click(function () {
   $('#house__active').show();
 });
+
+$('.1A1-71-b').click(function () {
+  $('.1A1-71').show();
+});
+
+$('.1A2-81-b').click(function () {
+  $('.1A2-81').show();
+});
+
+$('.1A3-88-b').click(function () {
+  $('.1A3-88').show();
+});
+
+$('.1A3-88-b').click(function () {
+  $('.1A3-88').show();
+});
+
+
+$('.1A3-95-b').click(function () {
+  $('.1A3-95').show();
+});
+
+
+$('.1A4-107-b').click(function () {
+  $('.1A4-107').show();
+});
+
+
+$('.1A5-125-b').click(function () {
+  $('.1A5-125').show();
+});
+
+
+$('.1A5-126-b').click(function () {
+  $('.1A5-126').show();
+});
+
+$('.1B1-83-b').click(function () {
+  $('.1B1-83').show();
+});
+
+
+$('.1B2-91-b').click(function () {
+  $('.1B2-91').show();
+});
+
+
+$('.1B3-101-b').click(function () {
+  $('.1B3-101').show();
+});
+
+
+$('.1B4-105-b').click(function () {
+  $('.1B4-105').show();
+});
+
+
+$('.1B5-112-b').click(function () {
+  $('.1B5-112').show();
+});
+
+
+$('.1B6-120-b').click(function () {
+  $('.1B6-120').show();
+});
+
+
+$('.1B7-125-b').click(function () {
+  $('.1B7-125').show();
+});
+
+
+$('.1B8-128-b').click(function () {
+  $('.1B8-128').show();
+});
+
+
+$('.1П1-103-b').click(function () {
+  $('.1П1-103').show();
+});
+
+
+$('.1П2-113-b').click(function () {
+  $('.1П2-113').show();
+});
+
+$('.2A1-116-b').click(function () {
+  $('.2A1-116').show();
+});
+
+$('.2A2-121-b').click(function () {
+  $('.2A2-121').show();
+});
+
+
+$('.2B1-122-b').click(function () {
+  $('.2B1-122').show();
+});
+
+
+$('.2B2-137-b').click(function () {
+  $('.2B2-137').show();
+});
+
+
+$('.2B3-134-b').click(function () {
+  $('.2B3-134').show();
+});
+
+
+$('.2B3-146-b').click(function () {
+  $('.2B3-146').show();
+});
+
+
+$('.2B4-160-b').click(function () {
+  $('.2B4-160').show();
+});
+
+
+$('.2B6-142-b').click(function () {
+  $('.2B6-142').show();
+});
+
+
+$('.2П1-124-b').click(function () {
+  $('.2П1-124').show();
+});
+
+
